6affc2df5ef6fceca229b994fb6a47f7a4dd9c74
GustavoH1709 <gustavoh1709@hotmail.com>
2025-03-02 15:37:18 -0300

    Create DateField.tsx

---
+++ src/components/inputs/DateField.tsx
@@ -0,0 +1,26 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+interface DateInputProps {
+  label: string;
+  placeholder?: string;
+  error?: string;
+  register: UseFormRegisterReturn;
+}
+
+export const DateField = ({
+  label,
+  placeholder = '',
+  error,
+  register,
+}: DateInputProps) => {
+  return (
+    <>
+      <Label>{label}</Label>
+      <Input type="date" placeholder={placeholder || label} {...register} />
+      {error && <p className="mt-0 text-sm text-red-500">{error}</p>}
+    </>
+  );
+};
