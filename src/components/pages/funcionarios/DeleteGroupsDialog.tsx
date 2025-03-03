9d6f1c5cd33f82c36288c3dd23e48f6e079cf8b5
GustavoH1709 <gustavoh1709@hotmail.com>
2025-03-02 22:04:33 -0300

    Update DeleteGroupsDialog.tsx

--- src/components/pages/funcionarios/DeleteGroupsDialog.tsx
+++ src/components/pages/funcionarios/DeleteGroupsDialog.tsx
@@ -1,20 +1,11 @@
-
 import { Trash2 } from 'lucide-react';
+import { useState } from 'react';
 
+import { ConfirmationDialog } from '@/components/modals/ConfirmationDialog';
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogFooter,
-} from '@/components/ui/dialog';
 import { useDeleteGrupos } from '@/hooks/api/grupo/useDeleteGrupos';
 import { useToast } from '@/hooks/use-toast';
 import { MessagesResource } from '@/i18n/resources';
-import { ConfirmationDialog } from '@/components/modals/ConfirmationDialog';
-import { useState } from 'react';
 
 interface DeleteGroupsDialogProps {
   selected: string[];
@@ -42,33 +33,10 @@ export const DeleteGroupsDialog = ({
   };
 
   const confirmDeleteGroups = () => {
-    deleteGrupos.mutateAsync({ ListaGrupos: selected }).then(
-      (success) => {
-        if (success) {
-          toast({
-            title: MessagesResource.SUCCESS,
-            description: MessagesResource.GROUPS_DELETED_SUCCESSFULLY,
-          });
-          onDeleteSuccess();
-        } else {
-          toast({
-            title: MessagesResource.ERROR,
-            description: MessagesResource.ERROR_DELETING_GROUPS,
-            variant: 'destructive',
-          });
-        }
-        setConfirmModalOpen(false);
-      },
-      (error) => {
-        console.error('Error deleting groups:', error);
-        toast({
-          title: MessagesResource.ERROR,
-          description: MessagesResource.ERROR_DELETING_GROUPS,
-          variant: 'destructive',
-        });
-        setConfirmModalOpen(false);
-      }
-    );
+    deleteGrupos.mutateAsync({ ListaGrupos: selected }).then(() => {
+      setConfirmModalOpen(false);
+      onDeleteSuccess();
+    });
   };
 
   return (
