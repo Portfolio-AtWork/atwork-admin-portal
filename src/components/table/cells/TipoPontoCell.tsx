e6eed32303a573f3a82651d45a768578cbb94e92
GustavoH1709 <gustavoh1709@hotmail.com>
2025-06-04 20:49:42 -0300

    Update TipoPontoCell.tsx

--- src/components/table/cells/TipoPontoCell.tsx
+++ src/components/table/cells/TipoPontoCell.tsx
@@ -5,7 +5,7 @@ interface TipoPontoCellProps {
   TP_Ponto: string;
 }
 
-function getLabel(tp_ponto: string) {
+function formatName(tp_ponto: string) {
   switch (tp_ponto) {
     case 'E':
       return MessagesResource.ENTRADA;
@@ -17,7 +17,7 @@ function getLabel(tp_ponto: string) {
 }
 
 export const TipoPontoCell = ({ TP_Ponto }: TipoPontoCellProps) => {
-  const label = getLabel(TP_Ponto);
+  const label = formatName(TP_Ponto);
 
   return <TableCell>{label}</TableCell>;
 };
